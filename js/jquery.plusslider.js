a7b35d6388ae1518b9c39a8562d6b70edaa47dff
Jamy Golden <jamyg88@gmail.com>
2011-08-08 23:14:39 +0200

    toSlide() is now the primary function. nextSlide() has been removed.

--- js/jquery.plusslider.js
+++ js/jquery.plusslider.js
@@ -21,40 +21,36 @@
         
         base.init = function(){
             base.options = $.extend({}, $.plusSlider.defaults, options);
+            // Vasic variables & Injected HTML elements 
             base.$slides = base.$el.children();
             base.$totalSlides = base.$slides.length;
-            
-            // Injected HTML elements 
             base.$el.wrap('<div class="plusSlider ' + base.$el.attr('id') + '" />');
+            base.$wrap = base.$el.parent();
             base.$slides.addClass('child');
             base.$slides.eq(0).addClass('current');
             
             // Slider/Fader Settings
             if(base.options.sliderType == 'slider'){
-                base.$el.parent().addClass('plusTypeSlider');
+                base.$wrap.addClass('plusTypeSlider');
                 base.$slideWidth = base.$el.find(':first').outerWidth(true);
                 base.$sliderWidth = base.$slideWidth * base.$totalSlides;
                 base.$stopPosition = base.$sliderWidth - base.$slideWidth;
                 base.$el.width(base.$sliderWidth);
-                base.$el.children().show();
+                base.$slides.show();
             }else{
-                base.$el.parent().addClass('plusTypeFader');
-                base.$el.children(':first').show();
+                base.$wrap.addClass('plusTypeFader');
+                base.$slides.eq(0).show();
             }
             if(base.$totalSlides === 1){
                 base.options.autoPlay = false;
                 base.options.createArrows = false;
                 base.options.createPagination = false;
             }
-            
+
             // Overide default CSS width
-             if(base.options.width){
-                base.$el.parent().width(base.options.width);
-             }
-             // Overide default CSS height
-             if(base.options.height){
-                base.$el.parent().height(base.options.height);
-             }
+            if(base.options.width) base.$wrap.width(base.options.width);
+            // Overide default CSS height
+            if(base.options.height) base.$wrap.height(base.options.height);
             // Begin settings.pagination
             if(base.options.createPagination){
                 // #slider-controls
@@ -62,10 +58,10 @@
                     base.$el.before('<div class="plusSlider-controls" />');
                     base.$sliderControls = base.$el.prev('.plusSlider-controls');
                 } else{
-                       base.$el.after('<div class="plusSlider-controls" />');
-                       base.$sliderControls = base.$el.next('.plusSlider-controls');
-                   }
-                   base.$sliderControls.wrap('<div class="plusSlider-controls-wrapper" />');
+                   base.$el.after('<div class="plusSlider-controls" />');
+                   base.$sliderControls = base.$el.next('.plusSlider-controls');
+                }
+                base.$sliderControls.wrap('<div class="plusSlider-controls-wrapper" />');
                 // Pagination
                 for (var i = 0; i < base.$totalSlides; i++){
                     base.$sliderControls.append('<a href="#" rel="' + i + '">' + (i + 1) + '</a>');
@@ -79,86 +75,37 @@
                 });
                 base.$sliderControls.find('a').eq(0).addClass('current');
             } // End settings.pagination
+
+            // Add plusSlider data
+            base.$el.data("slides",{ current: base.$el.children('.current').index() })
+            base.$wrap.before(base.$el.data("slides").current);
             
             // Begin Functions
-            base.nextSlide = function (direction){
-                if(direction !== false){
-                    direction = true;
-                }
+            base.toSlide = function (slide){
+                if(slide == 'next' || slide == null) slide = base.$el.data("slides").current + 1;
+                if(slide == 'prev') slide = base.$el.data("slides").current - 1;
+                if(slide >= base.$totalSlides) slide = 0;
+                if(slide <= -1) slide = base.$totalSlides -1;
+
                 if(base.options.sliderType == 'slider'){
-                    if(direction == true && !base.$el.is(':animated')){
-                        //stop position -1 to fix IE8 inconsistancy
-                        if(base.$el.position().left > (base.$stopPosition - 1) * -1){
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a.current').removeClass('current').next().addClass('current');
-                            }
-                            base.$el.animate({
-                                left: "-=" + base.$slideWidth + "px"
-                            }, base.options.speed, base.options.sliderEasing);
-                            base.$el.children('.current').removeClass('current').next().addClass('current');
-                        }else{
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a:last').removeClass('current').siblings('a:first').addClass('current');
-                            }
-                            base.$el.animate({
-                                left: 0
-                            }, base.options.speed, base.options.sliderEasing);
-                            base.$el.children('.current').removeClass('current').siblings(':first').addClass('current');
+                    if(!base.$el.is(':animated')){
+                        if(base.options.createPagination){
+                            base.$sliderControls.find('a[rel="' + slide + '"]').addClass('current').siblings().removeClass('current');
                         }
-                    }else if(direction == false && !base.$el.is(':animated')){
-                        if(base.$el.position().left < 0 && !base.$slides.is(':animated')){
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a.current').removeClass('current').prev().addClass('current');
-                            }
-                            base.$el.animate({
-                                left: "+=" + base.$slideWidth + "px"
-                            }, base.options.speed, base.options.sliderEasing);
-                            base.$el.children('.current').removeClass('current').prev().addClass('current');
-                        }else{
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a:first').removeClass('current').siblings('a:last').addClass('current');
-                            }
-                            base.$el.animate({
-                                left: "-=" + base.$stopPosition + "px"
-                            }, base.options.speed, base.options.sliderEasing);
-                            base.$el.children('.current').removeClass('current').siblings(':last').addClass('current');
-                        }
+                        base.$el.animate({
+                            left: base.$slideWidth * slide * -1 + "px"
+                        }, base.options.speed, base.options.sliderEasing);
+                        base.$el.children('.current').removeClass('current').parent().children().eq(slide).addClass('current');
                     }
-                }else{
-                    if(direction == true && !base.$slides.is(':animated')){
-                        if(base.$el.children(':last').is('.current')){
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a:last').removeClass('current').siblings('a:first').addClass('current');
-                            }
-                            base.$el.children('.current').removeClass('current').css('zIndex', 60).siblings().css('zIndex', 50);
-                            base.$el.children(':first').css('zIndex', 100).addClass('current').fadeIn(base.options.speed, function(){
-                                base.$slides.not('.current').hide();
-                            });
-                        }else{
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a.current').removeClass('current').next().addClass('current');
-                            }
-                            base.$el.children('.current').removeClass('current').css('zIndex', 60).siblings().css('zIndex', 50).end().next().css('zIndex', 100).addClass('current').fadeIn(base.options.speed, function(){
-                                base.$slides.not('.current').hide();
-                            });
+                }else{ // Begin Fader
+                    if(!base.$slides.is(':animated')){
+  
+                        if(base.options.createPagination){
+                            base.$sliderControls.find('a[rel="' + slide + '"]').addClass('current').siblings().removeClass('current');
                         }
-                    }else if(direction == false && !base.$slides.is(':animated')){
-                        if(base.$el.children(':first').is('.current')){
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a:first').removeClass('current').siblings('a:last').addClass('current');
-                            }
-                            base.$el.children('.current').removeClass('current').css('zIndex', 60).siblings().css('zIndex', 50);
-                            base.$el.children(':last').css('zIndex', 100).addClass('current').fadeIn(base.options.speed, function(){
-                                base.$slides.not('.current').hide();
-                            });
-                        }else if(!base.$slides.is(':animated')){
-                            if(base.options.createPagination){
-                                base.$sliderControls.find('a.current').removeClass('current').prev().addClass('current');
-                            }
-                            base.$el.children('.current').removeClass('current').siblings().css('zIndex', 50).end().css('zIndex', 60).prev().css('zIndex', 100).addClass('current').fadeIn(base.options.speed, function(){
-                                base.$slides.not('.current').hide();
-                            });
-                        }
+                        base.$slides.eq(slide).css('zIndex', 100).addClass('current').fadeIn(base.options.speed, function(){
+                            base.$slides.not('.current').hide();
+                        }).siblings().css('zIndex', 50).removeClass('current');
                     }
                 }
                 // Clear Timer
@@ -170,37 +117,10 @@
                 if (base.options.onSlide && typeof(base.options.onSlide) == "function") {
                     base.options.onSlide(base.$slides.filter('.current').index());
                 }
+
+                base.$el.data("slides",{ current: base.$el.children('.current').index() })
+                base.$wrap.before(base.$el.data("slides").current);
             }
-            
-            base.toSlide = function(index){
-                // Don't animate while animated
-                if(!base.$slides.is(':animated')){
-                    if(base.options.createPagination){
-                        base.$sliderControls.find('a[rel="' + index + '"]').addClass('current').siblings().removeClass('current');
-                    }
-                    if(base.options.sliderType == 'slider'){
-                        base.$el.animate({
-                            left: base.$slideWidth * index * -1 + "px"
-                        }, base.options.speed, base.options.sliderEasing);
-                        base.$el.children('.current').removeClass('current').parent().children().eq(index).addClass('current');
-                    }else{
-                        base.$slides.eq(index).siblings().css('zIndex', 50).removeClass('current');
-                        base.$slides.eq(index).css('zIndex', 100).addClass('current').fadeIn(base.options.speed, function(){
-                            base.$slides.not('.current').hide();
-                        });
-                    }
-                    // Clear Timer
-                    if(base.options.autoPlay){
-                        base.clearTimer();
-                        base.beginTimer();
-                    }
-                    // Callback
-                    if (base.options.onSlide && typeof(base.options.onSlide) == "function") {
-                        base.options.onSlide(index);
-                    }
-                }
-            }
-            // End Functions
             
             // Auto Play Begins
             if(base.options.autoPlay){
@@ -212,7 +132,7 @@
                 }
                 base.beginTimer = function(){
                     base.timer = window.setInterval(function(){
-                        base.nextSlide();
+                        base.toSlide('next');
                     }, base.options.displayTime);
                 }
                 base.beginTimer();
@@ -229,11 +149,10 @@
             // Create Arrows
             if(base.options.createArrows){
                 base.$el.before('<a class="arrow prev" href="#">prev</a><a class="arrow next" href="#">next</a>').siblings('.next').click(function(){
-                    base.nextSlide();
+                    base.toSlide('next');
                     return false;
-                });
-                base.$el.siblings('.prev').click(function(){
-                    base.nextSlide(false);
+                }).siblings('.prev').click(function(){
+                    base.toSlide('prev');
                     return false;
                 });
             } // End Arrow Creation
@@ -247,20 +166,39 @@
                 $(window).keyup(function(e){
                     if(base.$el.is('.activePlusSlider')){
                         if(e.keyCode == 39){
-                            base.nextSlide();
+                            base.toSlide();
                         }else if(e.keyCode == 37){
-                            base.nextSlide(false);
+                            base.toSlide('prev');
                         }
                     }
                 });
             } // End Keyboard navigation
 
-            // Pagination Titles
+            // Pagination Titles-
             if(base.options.paginationTitle){
                 for (var i = 0; i < base.$totalSlides; i++){
                     base.$sliderControls.find('a[rel="' + i + '"]').text(base.$slides.eq(i).attr('data-title'));
                 }
             } // End Pagination Titles
+
+            // Pagination Thumbnails - FIREFOX 5+ ONLY
+            if(base.options.paginationThumbnails){
+
+                for(i = 0; i < base.$slides.length; i++){
+                    var $this = base.$slides.eq(i);
+                    if(!$this.attr('id')){
+                        $this.attr('id', 'plusSlider-slide-' + $this.index()); 
+                    }
+                }
+
+                base.$sliderControls.after($("<div />", {'class': "plusSlider-thumbnail"}));
+                var sliderThumbnail = base.$sliderControls.next('.plusSlider-thumbnail');
+
+                sliderThumbnail.css({'height':base.$el.height(), 'width':base.$wrap.width()}).siblings('.plusSlider-controls').find('a').hover(function(){
+                    sliderThumbnail.css('background-image', '-moz-element(#' + base.$slides.eq($(this).attr('rel')).attr('id') + ')').toggleClass('plusSlider-active');
+                });
+
+            } // End Pagination Thumbnails
 
         };
         // Run initializer
@@ -272,6 +210,7 @@
         paginationBefore: false, // Place the pagination above the slider within the HTML
         paginationWidth: true, // Automatically gives the pagination a dynamic width
         paginationTitle: false, // Checks for attribute 'data-title' on each slide and names the pagination accordingly
+        paginationThumbnails: false, // FIREFOX 5+ ONLY - Creates thumbnails of the slides once you hover over the pagination
         
         displayTime: 4000, // The amount of time the slide waits before automatically moving on to the next one. This requires 'autoPlay: true'
         speed: 500, // The amount of time it takes for a slide to fade into another slide
